afb12208b6cd63df92270b9362b844dcc606aa4c
maiconademirdemelo-art <maiconademirdemelo@gmail.com>
2026-04-11 16:53:50 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,17 +4,13 @@ const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// Mostra todos os arquivos disponíveis
 const dir = __dirname;
-console.log('Diretório atual:', dir);
-console.log('Arquivos:', fs.readdirSync(dir));
 
 app.get('/', (req, res) => {
-  const arquivo = path.join(dir, 'plano-futuro.html');
+  const arquivo = path.join(dir, 'plano-futuro (2).html');
   if (fs.existsSync(arquivo)) {
     res.sendFile(arquivo);
   } else {
-    // Lista o que existe para debug
     res.send('Dir: ' + dir + '<br>Arquivos: ' + fs.readdirSync(dir).join('<br>'));
   }
 });
