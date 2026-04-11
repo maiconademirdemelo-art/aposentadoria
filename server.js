e7e5f038d825b6f594b06046c0bbf189a4ed9a97
maiconademirdemelo-art <maiconademirdemelo@gmail.com>
2026-04-11 16:43:17 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,12 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'plano-futuro.html'));
+});
 
 app.use(express.static(__dirname));
 
-app.get('/', (req, res) => {
-  const files = [
-    'plano-futuro.html',
-    'plano-futuro',
-    'index.html'
-  ];
-  
-  for (const f of files) {
-    const full = path.join(__dirname, f);
-    if (fs.existsSync(full)) {
-      return res.sendFile(full);
-    }
-  }
-  
-  res.send('Arquivos na raiz: ' + fs.readdirSync(__dirname).join(', '));
-});
-
-app.listen(PORT, () => console.log('OK porta ' + PORT));
+app.listen(PORT, () => console.log('Rodando na porta ' + PORT));
