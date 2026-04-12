3ef6a0eb8cd15a22a966d5d1238936c4bc84e6bc
maiconademirdemelo-art <maiconademirdemelo@gmail.com>
2026-04-12 02:57:30 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static(path.join(__dirname)));
+
+app.get('*', (req, res) => {
+  const files = fs.readdirSync(__dirname);
+  const html = files.find(f => f.endsWith('.html'));
+  if (html) {
+    res.sendFile(path.join(__dirname, html));
+  } else {
+    res.send('Arquivos: ' + files.join(', '));
+  }
+});
+
+app.listen(PORT, () => console.log('OK ' + PORT));
